5cffb23c79e7e5923cf3e388aa412476044fd638
alfian.yuska <alfian.yuska@phiraka.com>
2023-06-08 13:41:27 +0700

    updatePostController

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -63,4 +63,56 @@ class PostController extends Controller
 
         return new PostResource(true, 'Detail Data Post!', $post);
     }
+
+    public function update(Request $request, $id)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+
+        ]);
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //find post by ID
+        $post = Post::find($id);
+
+        //check if image is not empty
+        if ($request->hasFile('image')) {
+            //upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //update post with new image
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        } else {
+            //update post without image
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        }
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Diubah', $post);
+    }
+
+    public function destroy($id)
+    {
+        //find post by ID
+        $post = Post::find($id);
+
+        //delete post
+        $post->delete();
+
+        //return response
+        return new PostResource(true, 'Data Post berhasil di hapus', null);
+    }
 }
